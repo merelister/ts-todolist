59787db4c8f9e5587abcfde9b1efe98c7a9c0547
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2023-02-18 16:24:03 -0500

    fix

--- app/src/models.ts
+++ app/src/models.ts
@@ -1,12 +1,11 @@
-class Task {
-    id: string;
-    name: string;
-    description?: string;
-    assignedTo: "unassigned";
-    priority: number;
+export class Task {
+  id: string;
+  name: string;
+  description?: string;
+  assignedTo: "unassigned";
+  priority: number;
 
-    constructor(name: string) {
-        this.name = name;
-    }
+  constructor(name: string) {
+    this.name = name;
+  }
 }
-
