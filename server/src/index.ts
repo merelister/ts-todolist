eca253b673d42fc6c4e6f76b1d38802d2f18fc6c
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2023-02-18 16:21:11 -0500

    Adjusted port

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,10 +2,12 @@
 import express from "express";
 const app = express();
 
+const port = 4000;
+
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(port, () => {
+  console.log("Server running on port " + port);
 });
